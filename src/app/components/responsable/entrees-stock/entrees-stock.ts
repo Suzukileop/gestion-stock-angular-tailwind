860f4ae73e09beb9ecce6ddf923743c0aba00c90
Suzukileop <leopardjuliocesar8@gmail.com>
2025-06-08 20:33:01 +0300

    test commit

--- src/app/components/responsable/entrees-stock/entrees-stock.ts
+++ src/app/components/responsable/entrees-stock/entrees-stock.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { HeaderComponent } from '../../shared/components/header/header.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { SidebarResponsableComponent } from '../sidebar-responsable.component';
 
 @Component({
   selector: 'entrees-stock',
   standalone: true,
-  imports: [CommonModule, RouterLink, HeaderComponent],
+  imports: [CommonModule, RouterLink, HeaderComponent, SidebarResponsableComponent],
   templateUrl: './entrees-stock.html',
   styleUrls: ['./entrees-stock.css']
 })
